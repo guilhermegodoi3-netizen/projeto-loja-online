1691a5f91f216827f9c8b86ec7f93b28d6a3f96e
PudimDeMulango <guilherme.godoi3@aluno.educa.go.gov.br>
2025-10-20 11:42:09 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const botoesAdicionar = document.querySelectorAll('.add-carrinho');
+const listaCarrinho = document.getElementById('lista-carrinho');
+const totalTexto = document.getElementById('total');
+carrinho = []
+
+botoesAdicionar.forEach(botao => {
+    botao.addEventListener('click', () => {
+        const nome = botao.getAttribute('data-nome')
+        const preco = botao.getAttribute('data-preco')
+        alert('o nome é' + nome + 'o valor é: ' + preco );
+    })
+});
