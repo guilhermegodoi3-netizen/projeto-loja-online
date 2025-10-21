00ad4f3c4c0808fc3094e34e916832a9952db4f7
PudimDeMulango <guilherme.godoi3@aluno.educa.go.gov.br>
2025-10-21 09:05:38 -0300

    Commit

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-const botoesAdicionar = document.querySelectorAll('.add-carrinho');
-const listaCarrinho = document.getElementById('lista-carrinho');
-const totalTexto = document.getElementById('total');
+const botoesAdicionar = document.querySelectorAll(".add-carrinho");
+const listaCarrinho = document.getElementById("lista-carrinho");
+const totalTexto = document.getElementById("total");
+
 carrinho = []
 
 botoesAdicionar.forEach(botao => {
-    botao.addEventListener('click', () => {
-        const nome = botao.getAttribute('data-nome')
-        const preco = botao.getAttribute('data-preco')
-        alert('o nome é' + nome + 'o valor é: ' + preco );
+    botao.addEventListener("click", () => {
+        const nome = botao.getAttribute("data-nome");
+        const preco = botao.getAttribute("data-preco");
+        alert("O nome é" + nome + " o valor é: " + preco);
     })
 });
